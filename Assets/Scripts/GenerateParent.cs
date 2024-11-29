731ee0da4e78b83068cac628bc7c065e128605bb
MauroNardone <dev.mauronardone@gmail.com>
2024-11-29 13:06:42 +0100

    create new movementBoard in gridmanager

--- Assets/Scripts/GenerateParent.cs
+++ Assets/Scripts/GenerateParent.cs
@@ -5,13 +5,13 @@ using UnityEngine.UIElements;
 
 public class GenerateParent : MonoBehaviour
 {
-    public static RectTransform canvasParent(GameObject gameObject, string name, RectTransform canvas)
+    public static GameObject canvasParent(GameObject gameObject, string name, RectTransform canvas)
     {
         gameObject = new GameObject($"{name}");
         gameObject.transform.SetParent(canvas);
         RectTransform boardTransform = gameObject.AddComponent<RectTransform>();
         boardTransform.anchoredPosition = new(0, 0);
         boardTransform.localScale = new(1, 1, 1);
-        return boardTransform;
+        return gameObject;
     }
 }
--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -8,22 +8,33 @@ public class GridManager : MonoBehaviour
 {
     [SerializeField] private Tile _tile;
     private List<Tile> _tiles = new List<Tile> { };
+    private List<Tile> _movementTiles = new List<Tile> { };
     [SerializeField] private RectTransform _canvas;
     [SerializeField] private int _rows;
     [SerializeField] private int _columns;
     [SerializeField] private float _offset = 265;
     private GameObject _board;
+    private GameObject _movementBoard;
 
     void Start()
     {
-        RectTransform boardTransform = GenerateParent.canvasParent(_board, "Board", _canvas);
+        GameObject board = GenerateParent.canvasParent(_board, "Board", _canvas);
         for (int col = 0; col < _columns; col++)
         {
             for (int row = 0; row < _rows; row++)
             {
-                _tiles.Add(generateTile(_tile, boardTransform, col, row));
+                _tiles.Add(generateTile(_tile, board.GetComponent<RectTransform>(), col, row));
             }
         }
+        GameObject movementBoard = GenerateParent.canvasParent(_movementBoard, "MovementBoard", _canvas);
+        for (int col = 0; col < _columns; col++)
+        {
+            for (int row = 0; row < _rows; row++)
+            {
+                _movementTiles.Add(generateTile(_tile, movementBoard.GetComponent<RectTransform>(), col, row));
+            }
+        }
+        movementBoard.SetActive(false);
     }
 
     private Tile generateTile(Tile tile, RectTransform parent, int column, int row)
